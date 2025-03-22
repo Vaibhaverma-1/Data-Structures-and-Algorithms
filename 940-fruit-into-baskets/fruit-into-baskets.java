ad8c6274dae196559943cfc6fc4231d3c14db3c1
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2025-03-22 19:58:19 +0530

    Time: 51 ms (47.80%) | Memory: 55.7 MB (16.78%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -16,15 +16,15 @@ class Solution {
             map.put(fruits[r], freq);
 
             if (map.size() > 2) {
-                while (map.size() > 2) {
-                    int temp = map.get(fruits[l]);
-                    temp--;
-                    map.put(fruits[l], temp);
-                    if (temp == 0) {
-                        map.remove(fruits[l]);
-                    }
-                    l++;  // **Fixed: Increment left pointer**
+                
+                int temp = map.get(fruits[l]);
+                temp--;
+                map.put(fruits[l], temp);
+                if (temp == 0) {
+                    map.remove(fruits[l]);
                 }
+                l++;  // **Fixed: Increment left pointer**
+                
             }
             else{
             maxlen = Math.max(maxlen, r - l + 1);  // Always update max length
