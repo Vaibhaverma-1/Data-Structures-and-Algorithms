f05d322316f288bd5c08e6b47db96201e7d146ea
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2025-05-03 22:56:42 +0530

    Time: 87 ms (8.56%) | Memory: 44.6 MB (50.27%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        int n = lists.length;
+        if (n == 0) {
+            return null;
+        } else if (n == 1) {
+            return lists[0];
+        }
+
+        ListNode merged = null;
+        for (int i = 0; i < n; i++) {
+            merged = mergeTwoLists(merged, lists[i]);
+        }
+        return merged;
+    }
+
+    // Helper method to merge two sorted linked lists
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if (list1 == null) return list2;
+        if (list2 == null) return list1;
+
+        ListNode dummy = new ListNode(-1);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) current.next = list1;
+        if (list2 != null) current.next = list2;
+
+        return dummy.next;
+    }
+}
