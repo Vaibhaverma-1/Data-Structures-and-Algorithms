055e2026829c3c2ff4abac135235ec722afefff1
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2025-05-03 17:09:54 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (9.70%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.java
+++ 328-odd-even-linked-list/odd-even-linked-list.java
@@ -8,41 +8,22 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-import java.util.ArrayList;
-
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        if (head == null || head.next == null) {
+        if(head == null || head.next == null){
             return head;
         }
-
-        ArrayList<Integer> list = new ArrayList<>();
-        ListNode temp = head;
-        ListNode temp2 = head.next;
-
-        // Add odd-positioned nodes
-        while (temp != null) {
-            list.add(temp.val);
-            if (temp.next == null || temp.next.next == null) break;
-            temp = temp.next.next;
+        ListNode odd = head;
+        ListNode estart = head.next;
+        ListNode even = head.next;
+        while(odd.next != null && odd != null && even != null && even.next != null){
+            odd.next = odd.next.next;
+            odd = odd.next;
+            even.next = even.next.next;
+            even = even.next;
         }
-
-        // Add even-positioned nodes
-        while (temp2 != null) {
-            list.add(temp2.val);
-            if (temp2.next == null || temp2.next.next == null) break;
-            temp2 = temp2.next.next;
-        }
-
-        // Assign values back from the list
-        temp = head;
-        int i = 0;
-        int n = list.size();
-        while (temp != null && i < n) {
-            temp.val = list.get(i++);
-            temp = temp.next;
-        }
-
+        odd.next =estart;
         return head;
+
     }
 }
