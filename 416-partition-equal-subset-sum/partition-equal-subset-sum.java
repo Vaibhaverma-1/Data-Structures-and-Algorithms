ed02aa7bfb724a179414de851d74f9e2bfd49fa2
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2025-08-01 00:34:12 +0530

    Time: 45 ms (60.68%) | Memory: 46.7 MB (58.08%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,32 +1,30 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         int n = nums.length;
-        int totalSum = 0;
-
+        int sum = 0;
         for (int num : nums) {
-            totalSum += num;
+            sum += num;
         }
 
+        // If total sum is odd, can't divide equally
+        if (sum % 2 != 0) return false;
+        int target = sum / 2;
 
-        if (totalSum % 2 != 0) return false;
-
-        int target = totalSum / 2;
-
+        // dp[i][j] = true if we can form sum j using first i elements
         boolean[][] dp = new boolean[n + 1][target + 1];
 
-       
+        // Base case: sum 0 is always possible (empty subset)
         for (int i = 0; i <= n; i++) {
             dp[i][0] = true;
         }
 
+        // Fill the DP table
         for (int i = 1; i <= n; i++) {
-            for (int sum = 1; sum <= target; sum++) {
-                if (nums[i - 1] <= sum) {
-                    
-                    dp[i][sum] = dp[i - 1][sum] || dp[i - 1][sum - nums[i - 1]];
+            for (int j = 1; j <= target; j++) {
+                if (nums[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
                 } else {
-                    
-                    dp[i][sum] = dp[i - 1][sum];
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
