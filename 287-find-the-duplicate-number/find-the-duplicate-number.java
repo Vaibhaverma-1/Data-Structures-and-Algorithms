98f0aeeb71e48b3e00080d0e8e125e5835addcc8
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2025-08-03 01:48:00 +0530

    Time: 8 ms (38.59%) | Memory: 58.6 MB (23.51%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,19 +1,53 @@
-// Tortoise and Hare Method
 class Solution {
     public int findDuplicate(int[] nums) {
-        int slow = nums[0];
-        int fast = nums[0];
-        slow=nums[slow];
-        fast=nums[nums[fast]];
-        while(slow!=fast){
-            slow=nums[slow];
-            fast=nums[nums[fast]];
+        int n = nums.length;
+        int i=0;
+          while (i < n) {
+            int correct = nums[i] - 1;                    
+            if (nums[i] < 1 || nums[i] > n) {            
+                i++;
+            } else if (nums[i] != nums[correct]) {  
+                   
+                swap(nums, i, correct);
+            } else {
+                
+                i++;     
+                                               
+            }
         }
-        slow=nums[0];
-        while(slow!=fast){
-            slow=nums[slow];
-            fast=nums[fast];
+
+        for(int j=1; j<=n; j++){
+           if(nums[j-1] != j){
+            return nums[j-1];
+           }
         }
-        return slow;
+        return -1;
+        
+    }
+
+    void swap(int[] arr, int i, int j){
+        int t= arr[i];
+        arr[i]=arr[j];
+        arr[j]=t;
     }
 }
+
+// // Tortoise and Hare Method
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         int slow = nums[0];
+//         int fast = nums[0];
+//         slow=nums[slow];
+//         fast=nums[nums[fast]];
+//         while(slow!=fast){
+//             slow=nums[slow];
+//             fast=nums[nums[fast]];
+//         }
+//         slow=nums[0];
+//         while(slow!=fast){
+//             slow=nums[slow];
+//             fast=nums[fast];
+//         }
+//         return slow;
+//     }
+// }
