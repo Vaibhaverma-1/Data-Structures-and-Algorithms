fae2c3e385ed45d609bfa5997ffb88fb6d7f07ff
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2025-08-03 01:00:10 +0530

    Time: 1 ms (34.74%) | Memory: 45.9 MB (7.09%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n =nums.length;
+        int i=0;
+        while(i<n){
+            int correct = nums[i];
+            if(nums[i] == n) i++; 
+            else  if(nums[i] != nums[correct]){
+                swap(nums, i, correct);
+            }
+            else{
+                i++;
+            }
+        }
+        for(int j=0; j<n;j++){
+            if(nums[j]!=j){
+                return j;
+            }
+        }
+        return n;
+    }
+
+    void swap(int[] arr, int i, int j){
+        int t= arr[i];
+        arr[i]=arr[j];
+        arr[j]=t;
+    }
+}
