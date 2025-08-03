79322387ccba477da15f0f4c358eb92a6f46527c
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2025-08-03 23:50:48 +0530

    Time: 4 ms (98.93%) | Memory: 42.7 MB (79.41%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -7,29 +7,29 @@ class Solution {
         for (int i = 0; i < m; i++) need[s1.charAt(i) - 'a']++;
 
         int[] have = new int[26];
-        int required = m; // how many characters still needed to match s1's multiset
-
         int l = 0, r = 0;
-        while (r < n) {
-            int in = s2.charAt(r) - 'a';
-            if (need[in] > 0) {
-                if (have[in] < need[in]) required--; // we satisfied one needed char
-                have[in]++;
-            }
-            r++;
 
-            // keep window size at most m
-            while (r - l > m) {
-                int out = s2.charAt(l) - 'a';
-                if (need[out] > 0) {
-                    if (have[out] <= need[out]) required++; // we lose a needed char
-                    have[out]--;
-                }
-                l++;
+        while (l + m <= n) {
+            // grow window to size m
+            while (r < n && r - l + 1 <= m) {
+                have[s2.charAt(r) - 'a']++;
+                r++;
             }
 
-            if (required == 0) return true; // window of size m contains a permutation
+            // check if counts match
+            if (isMatch(need, have)) return true;
+
+            // slide window by one
+            have[s2.charAt(l) - 'a']--;
+            l++;
         }
         return false;
+    }
+
+    private boolean isMatch(int[] a, int[] b) {
+        for (int i = 0; i < 26; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
     }
 }
