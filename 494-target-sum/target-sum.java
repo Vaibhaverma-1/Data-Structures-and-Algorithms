4bae640c319bfa22cd7d9880d9199a84931098c2
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2025-08-05 15:13:30 +0530

    Time: 6 ms (73.75%) | Memory: 44.4 MB (36.08%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,20 +1,27 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-  
+        int sum = 0;
+        for (int num : nums) sum += num;
 
-        return solve(nums, 0, target);
-    }
+        if ((sum + target) % 2 != 0 || Math.abs(target) > sum) return 0;
 
-    int solve(int[] nums, int index, int target) {
-        if (index == nums.length) {
-            return target == 0 ? 1 : 0;
+        int find = (sum + target) / 2;
+
+        int[][] dp = new int[n + 1][find + 1];
+
+        // Base case: one way to make sum 0
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= find; j++) {
+                if (nums[i - 1] <= j)
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
         }
 
-        // Try both + and -
-        int add = solve(nums, index + 1, target - nums[index]);
-        int subtract = solve(nums, index + 1, target + nums[index]);
-
-        return add + subtract;
+        return dp[n][find];
     }
 }
