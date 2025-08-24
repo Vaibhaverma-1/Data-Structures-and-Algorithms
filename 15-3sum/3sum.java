a530f60db558db0d5760b9edf081520558d4f097
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2025-08-24 23:35:05 +0530

    Time: 293 ms (20.74%) | Memory: 52.2 MB (27.76%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,29 +1,29 @@
-import java.util.*;
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
+        List<List<Integer>> list = new ArrayList<>();
         int n = nums.length;
 
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip dup anchors
-            int l = i + 1, r = n - 1;
+        for (int i = 0; i < n - 1; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates for i
+            if (nums[i] > 0) break;
 
-            while (l < r) {
-                long sum = (long) nums[i] + nums[l] + nums[r]; // avoid overflow
-                if (sum == 0) {
-                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                    int lv = nums[l], rv = nums[r];
-                    while (l < r && nums[l] == lv) l++; // skip dup lefts
-                    while (l < r && nums[r] == rv) r--; // skip dup rights
-                } else if (sum < 0) {
-                    l++;
+            HashSet<Integer> set = new HashSet<>();
+            for (int j = i + 1; j < n; j++) {
+                int k = -(nums[i] + nums[j]);
+                if (set.contains(k)) {
+                    list.add(Arrays.asList(nums[i], nums[j], k));
+
+                    // skip duplicates for j inside for loop
+                    while (j + 1 < n && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
                 } else {
-                    r--;
+                    set.add(nums[j]);
                 }
             }
         }
-        return res;
+
+        return list;
     }
 }
