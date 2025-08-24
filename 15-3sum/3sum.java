8f8e3d12e186bf0e40721838a06b5917aabfb7c3
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2025-08-25 00:09:12 +0530

    Time: 31 ms (56.56%) | Memory: 51.2 MB (97.16%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -4,26 +4,28 @@ class Solution {
         List<List<Integer>> list = new ArrayList<>();
         int n = nums.length;
 
-        for (int i = 0; i < n - 1; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates for i
-            if (nums[i] > 0) break;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate i
 
-            HashSet<Integer> set = new HashSet<>();
-            for (int j = i + 1; j < n; j++) {
-                int k = -(nums[i] + nums[j]);
-                if (set.contains(k)) {
-                    list.add(Arrays.asList(nums[i], nums[j], k));
+            int target = -nums[i];               // target depends on i
+            int j = i + 1, k = n - 1;
 
-                    // skip duplicates for j inside for loop
-                    while (j + 1 < n && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
+            while (j < k) {
+                int pair = nums[j] + nums[k];    // compare pair to target
+                if (pair == target) {
+                    list.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    // skip duplicates for j and k
+                    while (j < k && nums[j] == nums[j + 1]) j++;
+                    while (j < k && nums[k] == nums[k - 1]) k--;
+                    j++;
+                    k--;
+                } else if (pair < target) {
+                    j++; // need larger pair
                 } else {
-                    set.add(nums[j]);
+                    k--; // need smaller pair
                 }
             }
         }
-
         return list;
     }
 }
