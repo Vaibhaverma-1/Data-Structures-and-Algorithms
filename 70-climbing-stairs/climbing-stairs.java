415cd9a64b84cbf9dbec92c6389e1230a0914e46
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2025-08-25 16:21:54 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (27.94%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,13 +1,13 @@
 class Solution {
     public int climbStairs(int n) {
-       int[] dp = new int[n + 2];   // indices: 0..n+1
-dp[n]   = 1;                 // base
-dp[n+1] = 0;                 // base
-for (int i = n - 1; i >= 0; i--) {
-    dp[i] = dp[i + 1] + dp[i + 2];   // max access is i = n-1 → n, n+1 (in-bounds)
-}
-return dp[0];
-
+        int[] dp = new int[n+1];
+        dp[0] =1;
+        if(n>=1){
+            dp[1]=1;
+        }
+        for(int i=2; i<=n; i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
-    
 }
