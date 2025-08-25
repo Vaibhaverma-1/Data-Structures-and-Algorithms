3be39039dc294da86d6b5020cc830f86fd7250ea
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2025-08-25 17:09:20 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (55.93%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -3,7 +3,7 @@ class Solution {
         int count = 0;
         while (n != 0) {
             if ((n & 1) == 1) count++;
-            n = n >>> 1; // always shift, logical shift for unsigned behavior
+            n = n >> 1; // always shift, logical shift for unsigned behavior
         }
         return count;
     }
