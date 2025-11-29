7395ba7a5741b70ce2067af02a645234417ba763
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2025-11-29 16:22:19 +0530

    Time: 6 ms (87.52%) | Memory: 43.6 MB (36.17%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,24 +1,20 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        HashMap <Character, Character> map1 = new HashMap<>();
-        HashMap <Character, Character> map2 = new HashMap<>();
-        for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
-            char d = t.charAt(i);
-            if(map1.containsKey(c)){
-                if(map1.get(c) != d) return false;
-            }
-            else{
-                map1.put(c, d);
-            }
+        if (s.length() != t.length()) return false;
 
+        int[] map1 = new int[256];  // s → t
+        int[] map2 = new int[256];  // t → s
 
-             if(map2.containsKey(d)){
-                if(map2.get(d) != c) return false;
-            }
-            else{
-                map2.put(d, c);
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+
+            if (map1[c1] == 0 && map2[c2] == 0) {
+                map1[c1] = c2;
+                map2[c2] = c1;
+            } 
+            else {
+                if (map1[c1] != c2 || map2[c2] != c1) return false;
             }
         }
         return true;
