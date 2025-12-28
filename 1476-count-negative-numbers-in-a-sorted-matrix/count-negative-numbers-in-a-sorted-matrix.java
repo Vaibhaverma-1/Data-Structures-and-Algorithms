ebb0c951517ba6992c12e399fef64080647d8896
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2025-12-28 14:15:11 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (7.96%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.java
@@ -2,14 +2,19 @@ class Solution {
     public int countNegatives(int[][] grid) {
      int n = grid.length;
      int m = grid[0].length;
-     int low=0;
-     int high = n-1;
+     
      int count=0;
-     for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
-            if(grid[i][j]<0){
-                count++;
-            }
+     int i=0;
+     int j=m-1;
+     while(i<n && j>=0)
+     { 
+        if(grid[i][j]<0){
+            count+=n-i;
+            
+            j--;
+        }
+        else{
+            i++;
         }
      }
      return count;
