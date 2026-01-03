352e9f3a4d39413e506c7f252b07e92b491cbe8c
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-01-03 15:59:39 +0530

    Time: 10 ms (75.10%) | Memory: 48 MB (9.54%) - LeetSync

--- 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.java
+++ 2609-distinct-prime-factors-of-product-of-array/distinct-prime-factors-of-product-of-array.java
@@ -5,14 +5,11 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             int rem = nums[i];
             for (int div = 2; div * div <= rem; div++) {
-                if (rem % div == 0) {
-                    set.add(div);
                     while (rem % div == 0) {
+                        set.add(div);
                         rem /= div;
                     }
-                }
             }
-
             if (rem > 1) {
                 set.add(rem);
             }
