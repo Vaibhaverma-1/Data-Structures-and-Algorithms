c8b7509c70cd23fe745994fe6c709e760f299341
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-01-05 11:10:34 +0530

    Time: 7 ms (64.01%) | Memory: 48.4 MB (18.59%) - LeetSync

--- 1254-deepest-leaves-sum/deepest-leaves-sum.java
+++ 1254-deepest-leaves-sum/deepest-leaves-sum.java
@@ -13,27 +13,47 @@
  *     }
  * }
  */
-class Solution{
+
+ class Solution{
     public int deepestLeavesSum(TreeNode root){
-        int depth=maxDepth(root);
         Queue<TreeNode> q=new LinkedList<>();
         q.offer(root);
-        int level=1,sum=0;
+        int sum=0;
         while(!q.isEmpty()){
             int n=q.size();
-            if(level==depth) sum=0;
+            sum=0;
             for(int i=0;i<n;i++){
                 TreeNode node=q.poll();
-                if(level==depth) sum+=node.val;
+                sum+=node.val;
                 if(node.left!=null) q.offer(node.left);
                 if(node.right!=null) q.offer(node.right);
             }
-            level++;
         }
         return sum;
     }
-    int maxDepth(TreeNode root){
-        if(root==null) return 0;
-        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
-    }
 }
+
+// class Solution{
+//     public int deepestLeavesSum(TreeNode root){
+//         int depth=maxDepth(root);
+//         Queue<TreeNode> q=new LinkedList<>();
+//         q.offer(root);
+//         int level=1,sum=0;
+//         while(!q.isEmpty()){
+//             int n=q.size();
+//             if(level==depth) sum=0;
+//             for(int i=0;i<n;i++){
+//                 TreeNode node=q.poll();
+//                 if(level==depth) sum+=node.val;
+//                 if(node.left!=null) q.offer(node.left);
+//                 if(node.right!=null) q.offer(node.right);
+//             }
+//             level++;
+//         }
+//         return sum;
+//     }
+//     int maxDepth(TreeNode root){
+//         if(root==null) return 0;
+//         return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+//     }
+// }
