2e14a10d6b365e4624be0422b3d3c0365a3ac3be
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-01-09 13:21:32 +0530

    Time: 3 ms (78.49%) | Memory: 44.5 MB (51.70%) - LeetSync

--- 3736-find-valid-pair-of-adjacent-digits-in-string/find-valid-pair-of-adjacent-digits-in-string.java
+++ 3736-find-valid-pair-of-adjacent-digits-in-string/find-valid-pair-of-adjacent-digits-in-string.java
@@ -1,21 +1,20 @@
 class Solution {
     public String findValidPair(String s) {
         int n = s.length();
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(int i=0; i<n; i++){
-            int freq=0;
-            if(map.containsKey(s.charAt(i)))
-            {
-                freq = map.get(s.charAt(i));
-            }
-            freq++;
-            map.put(s.charAt(i),freq);
-        }
-        for (int i = 0; i < n - 1; i++) {
-            if (map.get(s.charAt(i)) == s.charAt(i) - '0'
-             && map.get(s.charAt(i + 1)) == s.charAt(i + 1) - '0' && s.charAt(i)!=s.charAt(i+1)) {
+        int[] freq = new int[10];
 
-                return "" + s.charAt(i) + s.charAt(i+1);
+        // count digit frequency
+        for (int i = 0; i < n; i++) {
+            freq[s.charAt(i) - '0']++;
+        }
+
+        // check adjacent pair
+        for (int i = 0; i < n - 1; i++) {
+            int a = s.charAt(i) - '0';
+            int b = s.charAt(i + 1) - '0';
+
+            if (a != b && freq[a] == a && freq[b] == b) {
+                return "" + s.charAt(i) + s.charAt(i + 1);
             }
         }
         return "";
