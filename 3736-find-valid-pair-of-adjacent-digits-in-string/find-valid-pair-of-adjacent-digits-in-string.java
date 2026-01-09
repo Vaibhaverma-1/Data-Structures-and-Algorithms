1ce3617c65450e9a1e7d4bb91d9ae5146baa1f1c
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-01-09 13:22:35 +0530

    Time: 3 ms (78.49%) | Memory: 43.9 MB (93.58%) - LeetSync

--- 3736-find-valid-pair-of-adjacent-digits-in-string/find-valid-pair-of-adjacent-digits-in-string.java
+++ 3736-find-valid-pair-of-adjacent-digits-in-string/find-valid-pair-of-adjacent-digits-in-string.java
@@ -1,21 +1,11 @@
-class Solution {
-    public String findValidPair(String s) {
-        int n = s.length();
-        int[] freq = new int[10];
-
-        // count digit frequency
-        for (int i = 0; i < n; i++) {
-            freq[s.charAt(i) - '0']++;
-        }
-
-        // check adjacent pair
-        for (int i = 0; i < n - 1; i++) {
-            int a = s.charAt(i) - '0';
-            int b = s.charAt(i + 1) - '0';
-
-            if (a != b && freq[a] == a && freq[b] == b) {
-                return "" + s.charAt(i) + s.charAt(i + 1);
-            }
+class Solution{
+    public String findValidPair(String s){
+        int n=s.length();
+        int[] f=new int[10];
+        for(int i=0;i<n;i++)f[s.charAt(i)-'0']++;
+        for(int i=0;i<n-1;i++){
+            int a=s.charAt(i)-'0',b=s.charAt(i+1)-'0';
+            if(a!=b&&f[a]==a&&f[b]==b)return ""+s.charAt(i)+s.charAt(i+1);
         }
         return "";
     }
