a75cc113dbc1e7d7a697f4dea3d4434da4dffcd5
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-02-08 12:06:24 +0530

    Time: 63 ms (16.80%) | Memory: 48.1 MB (16.80%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,26 +1,41 @@
 class Solution {
     public int minimumDeletions(String s) {
-        int a_after = 0;
-        int b_before = 0;
-
-        // count total 'a'
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'a') {
-                a_after++;
+        int n = s.length();
+        int a_count=0, b_count=0;
+        for(int i=0; i<n; i++){
+            if(s.charAt(i)=='a'){
+               a_count++;
+            }
+            else{
+                b_count++;
             }
         }
-
-        int ans = a_after; // case: delete all 'a'
-
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == 'a') {
-                a_after--; // this 'a' moves from right to left
-            } else {
-                b_before++; // this 'b' is now on the left
+        int[] a = new int[n];
+        int[] b = new int[n];
+        b[0]=0;
+        for(int i=1; i<n; i++){
+            if(s.charAt(i-1)=='b'){
+            b[i]=b[i-1]+1;
             }
-            ans = Math.min(ans, b_before + a_after);
+            else{
+                b[i]=b[i-1];
+            }
+        } 
+        a[n-1]=0; 
+        for(int i=n-2; i>=0; i--){
+            if(s.charAt(i+1)=='a'){
+                a[i]=a[i+1]+1;
+            }
+            else{
+                a[i]=a[i+1];
+            }
         }
-
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<n; i++){
+           ans = Math.min(ans, a[i]+b[i]);
+        }
+    
         return ans;
+        
     }
 }
