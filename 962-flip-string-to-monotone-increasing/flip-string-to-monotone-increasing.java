c95929a16063a90b7f61b4d24d36d8680d9139b6
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-02-08 13:04:31 +0530

    Time: 14 ms (38.44%) | Memory: 46.9 MB (84.21%) - LeetSync

---
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int minFlipsMonoIncr(String s) {
+          int a_after = 0;
+        int b_before = 0;
+
+        // count total 'a'
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                a_after++;
+            }
+        }
+
+        int ans = a_after;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '0') {
+                a_after--;
+            } else {
+                b_before++;
+            }
+            ans = Math.min(ans, b_before + a_after);
+        }
+
+        return ans;
+    }
+}
