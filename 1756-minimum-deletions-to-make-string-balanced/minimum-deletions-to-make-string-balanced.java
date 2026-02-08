54a1c21f383551f6d69574aabc9e3ca5a6cbae2d
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-02-08 12:29:13 +0530

    Time: 134 ms (5.06%) | Memory: 48.1 MB (16.80%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.java
@@ -1,41 +1,17 @@
 class Solution {
     public int minimumDeletions(String s) {
         int n = s.length();
-        int a_count=0, b_count=0;
-        for(int i=0; i<n; i++){
-            if(s.charAt(i)=='a'){
-               a_count++;
-            }
-            else{
-                b_count++;
-            }
-        }
-        int[] a = new int[n];
-        int[] b = new int[n];
-        b[0]=0;
-        for(int i=1; i<n; i++){
-            if(s.charAt(i-1)=='b'){
-            b[i]=b[i-1]+1;
-            }
-            else{
-                b[i]=b[i-1];
-            }
-        } 
-        a[n-1]=0; 
-        for(int i=n-2; i>=0; i--){
-            if(s.charAt(i+1)=='a'){
-                a[i]=a[i+1]+1;
-            }
-            else{
-                a[i]=a[i+1];
+        int count = 0;
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+            if (!stack.isEmpty() && stack.peek() == 'b' && s.charAt(i) == 'a') {
+                count++;
+                stack.pop();
+            } else {
+                stack.push(s.charAt(i));
             }
         }
-        int ans=Integer.MAX_VALUE;
-        for(int i=0;i<n; i++){
-           ans = Math.min(ans, a[i]+b[i]);
-        }
-    
-        return ans;
-        
+        return count;
     }
 }
