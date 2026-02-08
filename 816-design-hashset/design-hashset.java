be92a63256423911c4d2839da4adda1c4c8a2372
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-02-08 17:23:37 +0530

    Time: 12 ms (98.67%) | Memory: 51.7 MB (77.09%) - LeetSync

--- 816-design-hashset/design-hashset.java
+++ 816-design-hashset/design-hashset.java
@@ -1,22 +1,78 @@
+class Node{
+    int val;
+    Node next;
+    Node( int val){
+        this.val = val;
+        this.next = null;
+    }
+    Node(int val,Node next){
+        this.val = val;
+        this.next = next;
+    }
+}
 class MyHashSet {
-    int[] set;
+    int n = 10000;
+    Node[] arr;
+    
     public MyHashSet() {
-       set=new int[1000001];
+       arr= new Node[n];
     }
     
     public void add(int key) {
-        set[key]=1;
+    int bucket = key % n;
+
+    if (arr[bucket] == null) {
+        arr[bucket] = new Node(key);
+        return;
     }
+
+    Node ptr = arr[bucket];
+    while (ptr.next != null) {
+        if (ptr.val == key) return;
+        ptr = ptr.next;
+    }
+
+    if (ptr.val == key) return;
+
+    ptr.next = new Node(key);
+}
+
     
-    public void remove(int key) {
-        if(contains(key)){
-            set[key]=0;
-        }
+ public void remove(int key) {
+    int bucket = key % n;
+    Node ptr = arr[bucket];
+
+    if (ptr == null) return;
+
+    if (ptr.val == key) {
+        arr[bucket] = ptr.next;
+        return;
     }
+
+    Node prev = ptr;
+    ptr = ptr.next;
+
+    while (ptr != null) {
+        if (ptr.val == key) {
+            prev.next = ptr.next;
+            return;
+        }
+        prev = ptr;
+        ptr = ptr.next;
+    }
+}
+
     
     public boolean contains(int key) {
-        if(set[key]==1){
-            return true;
+        int bucket = key %n;
+        Node ptr = arr[bucket];
+        if(ptr==null) return false;
+
+        while(ptr != null){
+            if(ptr.val==key){
+                return true;
+            }
+            ptr= ptr.next;
         }
         return false;
     }
