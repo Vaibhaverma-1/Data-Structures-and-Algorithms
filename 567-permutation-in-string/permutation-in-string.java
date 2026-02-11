039ed16285a1dad9b85b434fd6e98f9e87885590
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-02-11 15:59:30 +0530

    Time: 5 ms (96.80%) | Memory: 44 MB (87.15%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,35 +1,26 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int m = s1.length(), n = s2.length();
-        if (m > n) return false;
+        int n = s1.length();
+        int m = s2.length();
+        int[] freq_s1 = new int[26];
+        for(int i=0; i<n; i++){
+            freq_s1[s1.charAt(i)-'a']++;
+        }
+        int[] freq_s2 = new int[26];
+        int left=0;
+        for(int right =0; right<m; right++){
+            freq_s2[s2.charAt(right)-'a']++;
 
-        int[] need = new int[26];
-        for (int i = 0; i < m; i++) need[s1.charAt(i) - 'a']++;
-
-        int[] have = new int[26];
-        int l = 0, r = 0;
-
-        while (l + m <= n) {
-            // grow window to size m
-            while (r < n && r - l + 1 <= m) {
-                have[s2.charAt(r) - 'a']++;
-                r++;
+            while(right-left+1>n){
+                freq_s2[s2.charAt(left)-'a']--;
+                left++;
+            }
+            if(right-left+1==n && Arrays.equals(freq_s1,freq_s2)){
+                return true;
             }
 
-            // check if counts match
-            if (isMatch(need, have)) return true;
 
-            // slide window by one
-            have[s2.charAt(l) - 'a']--;
-            l++;
         }
         return false;
-    }
-
-    private boolean isMatch(int[] a, int[] b) {
-        for (int i = 0; i < 26; i++) {
-            if (a[i] != b[i]) return false;
-        }
-        return true;
     }
 }
