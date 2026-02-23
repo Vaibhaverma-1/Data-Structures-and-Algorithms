0d441008d9ba9e6fd7379d875c38fda79f82fa3e
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-02-23 13:30:21 +0530

    Time: 370 ms (5.19%) | Memory: 106.6 MB (5.19%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,28 +1,36 @@
 class Solution {
+    
     public boolean hasAllCodes(String s, int k) {
+        HashSet<String> set = new HashSet<>();
+        // generateAll(set,k,0);
+        int n = s.length();
+        for(int i=0; i<=n-k; i++){
+            StringBuilder temp= new StringBuilder();
+            for(int j=i; j<i+k; j++){
+                temp.append(s.charAt(j));
 
-        if (k > s.length()) return false;
-
-        int need = 1 << k;
-        boolean[] seen = new boolean[need];
-        int hash = 0;
-        int mask = need - 1;
-        int count = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-
-            // build rolling hash
-            hash = ((hash << 1) & mask) | (s.charAt(i) - '0');
-
-            // start checking only when window >= k
-            if (i >= k - 1) {
-                if (!seen[hash]) {
-                    seen[hash] = true;
-                    count++;
-                }
             }
+            String val = temp.toString();
+            set.add(val);
+            temp.setLength(0);
         }
 
-        return count == need;
+        // System.out.println(set);
+        // return set.size()==(int)Math.pow(2,k)?true:false;
+        return set.size() == (1 << k)?true:false;
     }
+    // public void generateAll(HashSet<String> set, int k, int n){
+       
+    //    if(n==k){
+    //     set.add(sb.toString());
+    //     return;
+    //    }
+    //    sb.append('0');
+    //    generateAll(set,k, n+1);
+    //    sb.deleteCharAt(sb.length() - 1);
+    //    sb.append('1');
+    //    generateAll(set,k,n+1);
+    //    sb.deleteCharAt(sb.length() - 1);
+
+    // }
 }
