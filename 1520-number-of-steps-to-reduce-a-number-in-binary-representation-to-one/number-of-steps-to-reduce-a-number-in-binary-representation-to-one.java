62cc94f784cd1efd6d8e4b315c788f8ef99bfe07
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-02-27 12:41:54 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (32.28%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,26 +1,19 @@
 class Solution {
     public int numSteps(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        int count = 0;
+        int steps = 0;
+        int carry = 0;
 
-        while (sb.length() > 1) {
-            if (sb.charAt(sb.length() - 1) == '0') {
-                sb.deleteCharAt(sb.length() - 1);
+        for (int i = s.length() - 1; i > 0; i--) {
+            int bit = s.charAt(i) - '0';
+
+            if (bit + carry == 1) {
+                steps += 2;   // add 1 then divide
+                carry = 1;
             } else {
-                int i = sb.length() - 1;
-                while (i >= 0 && sb.charAt(i) == '1') {
-                    sb.setCharAt(i, '0');
-                    i--;
-                }
-                if (i >= 0) {
-                    sb.setCharAt(i, '1');
-                } else {
-                    sb.insert(0, '1');
-                }
+                steps += 1;   // just divide
             }
-            count++;
         }
 
-        return count;
+        return steps + carry;
     }
 }
