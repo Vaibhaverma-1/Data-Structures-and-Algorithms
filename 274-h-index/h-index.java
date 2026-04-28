f9f27df1057718b4f0ec562cfdb144c6f9c8727f
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-04-28 12:20:09 +0530

    Time: 6 ms (25.20%) | Memory: 43.4 MB (72.68%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+        int hMax=0;
+        for(int i=n-1; i>=0; i--){
+            if(citations[i]>=(n-i)){
+                hMax =(n-i);
+            }
+            else{
+                break;
+            }
+        }
+        return hMax;
+    }
+    
+}
