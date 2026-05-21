71597a5af8e1f2336975004032fd98e6d8eba2e1
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-05-21 14:31:35 +0530

    Time: 70 ms (49.37%) | Memory: 80.4 MB (75.04%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.java
@@ -1,28 +1,36 @@
 class Solution {
     public int longestCommonPrefix(int[] arr1, int[] arr2) {
+
         HashSet<Integer> set = new HashSet<>();
-        int n = arr1.length;
-        for(int i=0; i<n; i++){
-            String s = String.valueOf(arr1[i]);
-            for(int j=1; j<=s.length(); j++){
-                set.add(Integer.parseInt(s.substring(0,j)));
+
+        // Store all prefixes from arr1
+        for (int num : arr1) {
+
+            while (num > 0) {
+                set.add(num);
+                num /= 10;
             }
         }
-        int m = arr2.length;
-        int len=0;
-        for(int i=0; i<m; i++){
-            String s = String.valueOf(arr2[i]);
-            for(int j=1; j<=s.length(); j++){
-                int prefix = Integer.parseInt(s.substring(0,j));
-                if(!set.contains(prefix)){
+
+        int ans = 0;
+
+        // Check prefixes in arr2
+        for (int num : arr2) {
+
+            while (num > 0) {
+
+                if (set.contains(num)) {
+
+                    ans = Math.max(ans,
+                            String.valueOf(num).length());
+
                     break;
                 }
-                else{
-                    len = Math.max(len,j);
-                }
+
+                num /= 10;
             }
         }
-        return len;
 
+        return ans;
     }
 }
