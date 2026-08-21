7b8a5db74d370bd1bedf488383f71d016e7d5935
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-08-21 12:11:32 +0530

    Time: 2 ms (99.54%) | Memory: 44 MB (28.36%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -1,30 +1,49 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        long ans=0L;
-        HashSet<Long> set = new HashSet<>();
-        PriorityQueue<Long> pq = new PriorityQueue<>();
-        pq.add(1L);
-        set.add(1L);
-        while(n>0){
-            long curr =pq.poll();
-            ans=curr;
-            n--;
-            if(!set.contains(curr*2)){
-               pq.add(curr*2);
-               set.add(curr*2);
-            }
-            if(!set.contains(curr*3)){
-               pq.add(curr*3);
-               set.add(curr*3);
-            }
-            if(!set.contains(curr*5)){
-               pq.add(curr*5);
-               set.add(curr*5);
-            }
-
-
+        long[] arr = new long[n];
+        arr[0]=1;
+        int p2=0;
+        int p3=0;
+        int p5=0;
+        for(int i=1; i<n; i++){
+            long next2= arr[p2]*2;
+            long next3= arr[p3]*3;
+            long next5= arr[p5]*5;
+            arr[i] = Math.min(next2, Math.min(next3,next5));
+            if(arr[i]==next2) p2++;
+            if(arr[i]==next3) p3++;
+            if(arr[i]==next5) p5++;
         }
-        return (int)ans;
-        
+        return (int) arr[n-1];
     }
 }
+// class Solution {
+//     public int nthUglyNumber(int n) {
+//         long ans=0L;
+//         HashSet<Long> set = new HashSet<>();
+//         PriorityQueue<Long> pq = new PriorityQueue<>();
+//         pq.add(1L);
+//         set.add(1L);
+//         while(n>0){
+//             long curr =pq.poll();
+//             ans=curr;
+//             n--;
+//             if(!set.contains(curr*2)){
+//                pq.add(curr*2);
+//                set.add(curr*2);
+//             }
+//             if(!set.contains(curr*3)){
+//                pq.add(curr*3);
+//                set.add(curr*3);
+//             }
+//             if(!set.contains(curr*5)){
+//                pq.add(curr*5);
+//                set.add(curr*5);
+//             }
+
+
+//         }
+//         return (int)ans;
+        
+//     }
+// }
