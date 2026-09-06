9276f09dadeb9bcb3cf0099e75012259ea10d254
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-09-06 16:04:34 +0530

    Time: 37 ms (22.48%) | Memory: 59.9 MB (17.71%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,30 +1,39 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> list = new ArrayList<>();
+        List<List<Integer>>list = new ArrayList<>();
         int n = nums.length;
-
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate i
-
-            int target = -nums[i];               // target depends on i
-            int j = i + 1, k = n - 1;
-
-            while (j < k) {
-                int pair = nums[j] + nums[k];    // compare pair to target
-                if (pair == target) {
-                    list.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    // skip duplicates for j and k
-                    while (j < k && nums[j] == nums[j + 1]) j++;
-                    while (j < k && nums[k] == nums[k - 1]) k--;
-                    j++;
-                    k--;
-                } else if (pair < target) {
-                    j++; // need larger pair
-                } else {
-                    k--; // need smaller pair
-                }
+        for(int i=0; i<n; i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
             }
+            int j=i+1,k=n-1;
+            while(j<k){
+               if(nums[i]+nums[j]+nums[k]==0){
+                ArrayList<Integer>ans = new ArrayList<>();
+                ans.add(nums[i]);
+                ans.add(nums[j]);
+                ans.add(nums[k]);
+                list.add(ans);
+                j++;
+                k--;
+                while(j<n && nums[j]==nums[j-1]){
+                j++;
+            }
+               while(k<n-1 && k>0 && nums[k]==nums[k+1]){
+                k--;
+            }
+                
+               }
+               else if(nums[i]+nums[j]+nums[k]>0){
+                k--;
+               }
+               else{
+                j++;
+               }
+                
+            }
+
         }
         return list;
     }
