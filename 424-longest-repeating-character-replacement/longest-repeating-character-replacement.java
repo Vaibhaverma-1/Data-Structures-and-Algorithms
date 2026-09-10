6ae299e17fcec8bad17d2b284fa84c92b197a8d7
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-09-10 20:15:27 +0530

    Time: 14 ms (36.29%) | Memory: 46 MB (78.65%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,30 +1,26 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         int[] freq = new int[26];
-        int i=0,j=0,maxLen = Integer.MIN_VALUE;
-        int n = s.length();
+        int i=0,j=0,maxLen=0;
+        int n=s.length();
         while(j<n){
-            int id =s.charAt(j)-'A';
+            int id=s.charAt(j)-'A';
             freq[id]++;
-            int maxfreq=Integer.MIN_VALUE;
-            for(int l=0; l<26; l++){
+            int maxfreq=0;
+            for(int l=0;l<26;l++){
                 maxfreq=Math.max(maxfreq,freq[l]);
             }
-            int diff = j-i+1-maxfreq;
+            int diff=j-i+1-maxfreq;
             while(diff>k){
                 freq[s.charAt(i)-'A']--;
                 i++;
-                 maxfreq = 0;
-
-    for (int l = 0; l < 26; l++) {
-        maxfreq = Math.max(maxfreq, freq[l]);
-    }
-
-    diff = j - i + 1 - maxfreq;
+                maxfreq=0;
+                for(int l=0;l<26;l++){
+                    maxfreq=Math.max(maxfreq,freq[l]);
+                }
+                diff=j-i+1-maxfreq;
             }
-            if(diff<=k){
-                maxLen = Math.max(maxLen, j-i+1);
-            }
+            maxLen=Math.max(maxLen,j-i+1);
             j++;
         }
         return maxLen;
