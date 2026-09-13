71c5ac32bd5aba07ece70d8999bac5410f94ee5f
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-09-13 18:10:25 +0530

    Time: 66 ms (35.61%) | Memory: 47 MB (46.21%) - LeetSync

--- 864-image-overlap/image-overlap.java
+++ 864-image-overlap/image-overlap.java
@@ -1,35 +1,26 @@
 class Solution {
-    public int largestOverlap(int[][] img1, int[][] img2) {
+    public int largestOverlap(int[][] img1,int[][] img2) {
         int n=img1.length;
         ArrayList<int[]> one1=new ArrayList<>();
         ArrayList<int[]> one2=new ArrayList<>();
-
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(img1[i][j]==1){
-                    one1.add(new int[]{i,j});
-                }
-                if(img2[i][j]==1){
-                    one2.add(new int[]{i,j});
-                }
+                if(img1[i][j]==1) one1.add(new int[]{i,j});
+                if(img2[i][j]==1) one2.add(new int[]{i,j});
             }
         }
-
-        HashMap<String,Integer> map=new HashMap<>();
+        HashMap<Integer,Integer> map=new HashMap<>();
         int max=0;
-
         for(int[] a:one1){
             for(int[] b:one2){
-                int rowShift=b[0]-a[0];
-                int colShift=b[1]-a[1];
-
-                String key=rowShift+","+colShift;
-
-                map.put(key,map.getOrDefault(key,0)+1);
-                max=Math.max(max,map.get(key));
+                int row=b[0]-a[0];
+                int col=b[1]-a[1];
+                int key=(row+n)*(2*n-1)+(col+n-1);
+                int count=map.getOrDefault(key,0)+1;
+                map.put(key,count);
+                max=Math.max(max,count);
             }
         }
-
         return max;
     }
 }
