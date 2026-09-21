2233fd1ab4b3bb678816b38da3728770c34b882f
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-09-21 12:10:00 +0530

    Time: 5 ms (61.98%) | Memory: 82.4 MB (83.79%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,53 +1,18 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int n = nums.length;
-        int i=0;
-          while (i < n) {
-            int correct = nums[i] - 1;                    
-            if (nums[i] < 1 || nums[i] > n) {            
-                i++;
-            } else if (nums[i] != nums[correct]) {  
-                   
-                swap(nums, i, correct);
-            } else {
-                
-                i++;     
-                                               
+        int n = nums.length,slow=0,fast=0;
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow==fast){
+                break;
             }
         }
-
-        for(int j=1; j<=n; j++){
-           if(nums[j-1] != j){
-            return nums[j-1];
-           }
+        slow=0;
+        while(slow!=fast){
+            slow=nums[slow];
+            fast=nums[fast];
         }
-        return -1;
-        
-    }
-
-    void swap(int[] arr, int i, int j){
-        int t= arr[i];
-        arr[i]=arr[j];
-        arr[j]=t;
+        return slow;
     }
 }
-
-// // Tortoise and Hare Method
-// class Solution {
-//     public int findDuplicate(int[] nums) {
-//         int slow = nums[0];
-//         int fast = nums[0];
-//         slow=nums[slow];
-//         fast=nums[nums[fast]];
-//         while(slow!=fast){
-//             slow=nums[slow];
-//             fast=nums[nums[fast]];
-//         }
-//         slow=nums[0];
-//         while(slow!=fast){
-//             slow=nums[slow];
-//             fast=nums[fast];
-//         }
-//         return slow;
-//     }
-// }
