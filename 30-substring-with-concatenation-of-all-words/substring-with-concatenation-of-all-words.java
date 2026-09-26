c102134e2483452d22e750cf667f384c4a50ab6d
Vaibhav Verma <110332953+Vaibhaverma-1@users.noreply.github.com>
2026-09-26 15:10:01 +0530

    Time: 12 ms (47.84%) | Memory: 47.2 MB (76.80%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.java
@@ -1,66 +1,33 @@
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
-
-        List<Integer> ans = new ArrayList<>();
-
-        int wordLen = words[0].length();
-        int totalWords = words.length;
-
-        HashMap<String, Integer> original = new HashMap<>();
-
-        for (String word : words) {
-            original.put(word, original.getOrDefault(word, 0) + 1);
+        List<Integer> ans=new ArrayList<>();
+        int len=words[0].length(),n=words.length;
+        HashMap<String,Integer> map=new HashMap<>();
+        for(String word:words){
+            map.put(word,map.getOrDefault(word,0)+1);
         }
-
-        // Different possible offsets
-        for (int offset = 0; offset < wordLen; offset++) {
-
-            int i = offset;
-            int count = 0;
-
-            HashMap<String, Integer> current = new HashMap<>();
-
-            for (int j = offset; j + wordLen <= s.length(); j += wordLen) {
-
-                String word = s.substring(j, j + wordLen);
-
-                // Word isn't required at all
-                if (!original.containsKey(word)) {
-                    current.clear();
-                    count = 0;
-                    i = j + wordLen;
+        for(int offset=0;offset<len;offset++){
+            int i=offset,count=0;
+            HashMap<String,Integer> curr=new HashMap<>();
+            for(int j=offset;j+len<=s.length();j+=len){
+                String word=s.substring(j,j+len);
+                if(!map.containsKey(word)){
+                    curr.clear();
+                    count=0;
+                    i=j+len;
                     continue;
                 }
-
-                // Add word
-                current.put(word, current.getOrDefault(word, 0) + 1);
+                curr.put(word,curr.getOrDefault(word,0)+1);
                 count++;
-
-                // Too many occurrences of this word
-                while (current.get(word) > original.get(word)) {
-
-                    String leftWord = s.substring(i, i + wordLen);
-
-                    current.put(leftWord, current.get(leftWord) - 1);
-
-                    i += wordLen;
+                while(curr.get(word)>map.get(word)){
+                    String left=s.substring(i,i+len);
+                    curr.put(left,curr.get(left)-1);
+                    i+=len;
                     count--;
                 }
-
-                // We have exactly all required words
-                if (count == totalWords) {
-                    ans.add(i);
-
-                    // Move left once for next possible answer
-                    String leftWord = s.substring(i, i + wordLen);
-                    current.put(leftWord, current.get(leftWord) - 1);
-
-                    i += wordLen;
-                    count--;
-                }
+                if(count==n) ans.add(i);
             }
         }
-
         return ans;
     }
 }
